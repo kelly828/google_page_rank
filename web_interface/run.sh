fda1b7cdcf5210f09913a8aa2de622156641708c
Wengao Ye <elleryqueenhomels@hotmail.com>
2017-12-01 23:07:22 +0800

    Update run.sh

--- web_interface/run.sh
+++ web_interface/run.sh
@@ -2,6 +2,12 @@ rm ./data/result*
 cp ../results/result.csv ./data/result.csv
 
 python parse.py
-python -m SimpleHTTPServer 8000
 
-# open http://localhost:8000
+create_http_server() {
+    python -m SimpleHTTPServer 8000
+}
+
+create_http_server &
+
+sleep 1
+open http://localhost:8000
